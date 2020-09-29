cb25e93dbb9d7f359af5c0cfb3057a3d10dd8a27
Marta <marta.nunes.abreu@gmail.com>
2020-09-29 20:40:21 +0100

    changed deployment script

--- src/templates/project-page.js
+++ src/templates/project-page.js
@@ -1,16 +1,24 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import styled from "styled-components"
 
+
+const CustomMain = styled.main`
+  margin: 0 auto;
+  max-width: 70vw;
+`
 
 export default function BlogPost({ data }) {
   const post = data.markdownRemark
   return (
     <Layout>
+      <CustomMain>
       <div>
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
+      </CustomMain>
     </Layout>
   )
 }
