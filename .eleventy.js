de7531abbc43fbd9e9c5170215a8b4022d1e87ee
Marta <marta.nunes.abreu@gmail.com>
2021-09-15 09:10:39 +0100

    added config file to git

---
+++ .eleventy.js
@@ -0,0 +1,39 @@
+const { DateTime } = require("luxon");
+
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addCollection("allMySortedContent", function(collectionApi) {
+    return collectionApi.getAllSorted();
+    });
+
+    eleventyConfig.addCollection("tagList", function(collection) {
+        let tagSet = new Set();
+        collection.getAll().forEach(item => {
+          (item.data.tags || []).forEach(tag => tagSet.add(tag));
+        });
+        return tagSet;
+    });
+
+    eleventyConfig.addNunjucksFilter("limit", (arr, limit) => arr.slice(0, limit));
+
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+      return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    });
+
+    return {
+        templateFormats: ['njk', 'md', 'html'],
+        dir: {
+          input: "src/pages",
+          // ⚠️ This value is relative to your input directory.
+          includes: "_includes",
+          data: "_data",
+          output: "_site",
+          layouts: "../_includes/layouts"
+        }
+      }
+};
+
+exports.render = function(data) {
+    return `<ul>
+      ${data.collections.post.map(post => `<li>${post.data.title}</li>`).join("\n")}
+    </ul>`;
+};
